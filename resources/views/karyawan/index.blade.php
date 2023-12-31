f1ed65c6a3646bc494821c4068d8e4aafea0dc49
donarazhar <donarazhar@gmail.com>
2023-12-31 18:28:06 +0700

    Perub

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -46,7 +46,9 @@
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                             <path d="M12 5l0 14" />
                                             <path d="M5 12l14 0" />
-                                        </svg>Tambah Data</a>
+                                        </svg>
+                                        Tambah Data
+                                    </a>
                                 </div>
                             </div>
                             <div class="row mt-2">
